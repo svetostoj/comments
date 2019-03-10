f20af37b2220ce92b73e8fac0f2a41d493a9912a
SVETO <svetostoj@hotmail.com>
2019-03-10 17:04:31 +1100

    Final

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Faker from 'faker';
+import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// class App extends React.Component {
+//     render() {
+//         window.navigator.geolocation.getCurrentPosition(pos=>console.log(pos), err=>console.log(err))
+//         return <div>Latitude: </div>
+//     };
+// };
+const App = function (){
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    return (
+        <div className="ui container comments">
+        <ApprovalCard>
+        <CommentDetail author="Sam" timeAgo="Today 4:34 pm" content="Nice blog Post!" avatar={Faker.image.avatar()}/>
+        </ApprovalCard>
+
+        <  ApprovalCard>
+        <CommentDetail author="Alex" timeAgo="Today 3:22 pm" content="I like the subject!" avatar={Faker.image.avatar()}/>
+        </ApprovalCard>
+
+        <ApprovalCard>
+        <CommentDetail author="Jane" timeAgo="Yesterday 4:34 pm" content="Jerry ne seri!" avatar={Faker.image.avatar()}/ >
+        </ApprovalCard>
+
+        </div>
+    );
+};
+
+ReactDOM.render(<App/>, document.querySelector('#root'))
